6eabed2dbc19db9bc1f9bd39e05c09a54005bd64
maiersk <poilyoungman@foxmail.com>
2021-01-08 23:54:23 +0800

    添加session, postgres, sequelize

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,5 +1,14 @@
 import app from '../index'
+import sequelize from '../database/Sequelize'
 
-app.listen(3000, () => {
-  console.log(`listen: ${3000}`)
+sequelize.authenticate().then(async () => {
+  console.log('connection has been')
+  await sequelize.sync({ force: true })
+
+  await app.listen(3000, () => {
+    console.log(`listen: ${3000}`)
+  })
+}).catch((err) => {
+  console.log('unable to connect to the database: ', err)
+  process.exit()
 })
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Koa from 'koa'
-import { router, bodyParser } from './routes/index'
+import { session_cfg } from './config'
+import bodyParser from 'koa-bodyparser'
+import router from './routes/index'
+import session from 'koa-session'
 
 const app = new Koa()
 
 app.use(bodyParser())
+  .use(session(session_cfg, app))
   .use(router.routes())
 
 module.exports = app
