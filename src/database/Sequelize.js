7c0c7b52afaaf788a546d0e42a7dc69b7841c5ed
maiersk <poilyoungman@foxmail.com>
2021-01-08 23:55:48 +0800

    sequelize 数据库相关

---
+++ src/database/Sequelize.js
@@ -0,0 +1,25 @@
+import { db } from '../config'
+import { Sequelize } from 'sequelize'
+
+const { 
+  database,
+  username,
+  password,
+  host,
+  port,
+  dialect
+} = db
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  port,
+  dialect,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 3000,
+    idle: 10000
+  }
+})
+
+export default sequelize
