0d4c4bd4d1e286506df0e2cd42d31ac4a5bbb6cb
maiersk <poilyoungman@foxmail.com>
2021-02-03 23:04:11 +0800

    push

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,6 +1,7 @@
 import app from '../index'
-import { site } from '../config'
+import { site, db } from '../config'
 import sequelize from '../database/Sequelize'
+import { Client } from 'pg'
 
 sequelize.authenticate().then(async () => {
   console.log('connection has been')
@@ -9,8 +10,18 @@ sequelize.authenticate().then(async () => {
   await app.listen(site.port, () => {
     console.log(`listen: ${site.port}`)
   })
-}).catch((err) => {
+}).catch(async (err) => {
+  if (err.message.indexOf(db.database) !== -1) {
+    const client = new Client({
+      host: db.host,
+      port: db.port,
+      user: db.username,
+      password: db.password
+    })
+    await client.connect()
+    await client.query(`CREATE DATABASE ${db.database}`).catch(() => { })
+  }
   console.log('unable to connect to the database: ', err.message)
-  sequelize.close()
-  process.exit()
+  await sequelize.close()
+  await process.exit()
 })
