ec95776b409a8998a80a16700c75ae2050cf7644
maiersk <poilyoungman@foxmail.com>
2021-02-19 16:32:24 +0800

    fix

--- src/bin/www.js
+++ src/bin/www.js
@@ -3,24 +3,28 @@ import { site, db } from '../config'
 import sequelize from '../database/Sequelize'
 import { Client } from 'pg'
 
-sequelize.authenticate().then(async () => {
-  console.log('connection has been')
-  await sequelize.sync({ force: true })
+const client = new Client({
+  host: db.host,
+  port: db.port,
+  user: db.username,
+  password: db.password
+})
 
-  await app.listen(site.port, () => {
-    console.log(`listen: ${site.port}`)
-  })
-}).catch(async (err) => {
-  if (err.message.indexOf(db.database) !== -1) {
-    const client = new Client({
-      host: db.host,
-      port: db.port,
-      user: db.username,
-      password: db.password
-    })
-    await client.connect()
+client.connect().then(async () => {
+  try {
     await client.query(`CREATE DATABASE ${db.database}`).catch(() => { })
+    await sequelize.authenticate()
+    console.log('connection has been')
+
+    await sequelize.sync({ force: true })
+
+    await app.listen(site.port, () => {
+      console.log(`listen: ${site.port}`)
+    })
+  } catch (error) {
+    throw new Error(error.message)
   }
+}).catch(async (err) => {
   console.log('unable to connect to the database: ', err.message)
   await sequelize.close()
   await process.exit()
