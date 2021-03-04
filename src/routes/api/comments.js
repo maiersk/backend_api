8e29a3e251d20507dac1917cb0b0b960a1d1f9ee
maiersk <poilyoungman@foxmail.com>
2021-03-04 23:10:00 +0800

    push

--- src/routes/api/comments.js
+++ src/routes/api/comments.js
@@ -1,73 +1,72 @@
 import Router from 'koa-router'
-import { Comment } from '../../models'
+import { Comment, User } from '../../models'
 import { data, err, msg } from '../../lib/res_msg'
+import { Op } from 'sequelize'
 
 const comments = new Router()
 comments.prefix('/comments')
 
 comments.get('/', async (ctx, next) => {
-  try {
-    const { page = 0, count = 10 } = ctx.query
-    if (page < 0 || count < 0) { throw new Error('negative number') }
-    const [_comments, total] = await Promise.all([
-      Comment.findAll({
-        raw: true,
-        limit: count,
-        offset: page !== 0 ? page * count : page
-      }),
-      Comment.findAndCountAll({})
-    ])
+  const { page = 0, count = 10, query } = ctx.query
 
-    ctx.body = data(_comments, { page: +page, total: total.count })
+  try {
+    if (page < 0 || count < 0) { throw new Error('negative number') }
+
+    let _comment = await Comment.findAndCountAll({
+      where: query ? { title: { [Op.like]: `%${query}%` } } : {},
+      include: [
+        { model: User }
+      ],
+      limit: +count,
+      offset: page !== 0 ? page * +count : page
+    })
+
+    ctx.body = data(_comment.rows, {
+      page: +page,
+      total: _comment.count,
+      total_pages: Math.floor((_comment.count + +count - 1) / +count)
+    })
   } catch (error) {
     ctx.body = err(error.message)
   }
 })
 
-comments.get('/:userId', async (ctx, next) => {
-  const id = ctx.params.userId
-  if (!id) { return }
-
-  const comment = await Comment.findAll({ where: { id } })
-  if (comment) {
-    ctx.body = data(comment)
-  } else {
-    ctx.body = err('not find')
-  }
-})
-
 comments.post('/', async (ctx, next) => {
-  const userid = ctx.session?.user?.userId
-  const { content } = ctx.request.body
+  const userId = ctx.session?.user?.id ?? false
+  const { postId, content } = ctx.request.body
 
   try {
+    if (!userId) { throw new Error('no login') }
+
     const comment = await Comment.create({
-      userid,
+      userId,
+      postId,
       content
     })
 
     ctx.body = data(comment)
   } catch (error) {
-    ctx.body = err(err.message)
+    ctx.body = err(error.message)
   }
 })
 
 comments.put('/:id', async (ctx, next) => {
-  const id = ctx.session.userid
-  const content = ctx.request.body
+  const userId = ctx.session?.user?.id ?? false
+  const id = ctx.params.id
+  const { content } = ctx.request.body
 
   try {
+    if (!userId) { throw new Error('no login') }
+
     const comment = await Comment.findOne({
       where: { id }
     })
-    if (!comment) {
+    if (comment.userId !== userId) {
       throw new Error('you are not owner')
     }
-    await Comment.update({
-      content
-    }, {
-      where: { id }
-    })
+    comment.content = content
+
+    await comment.save()
   } catch (error) {
     ctx.body = err(error.message)
   }
